29e969925b6e6d6f8073e9330c887e6be80b66cc
marcoingrasci95 <marcoingrasci95@gmail.com>
2020-04-18 12:45:26 +0200

    verifica utente

--- src/main/java/wa/ServletUtente.java
+++ src/main/java/wa/ServletUtente.java
@@ -1,6 +1,10 @@
 package wa;
 
+import java.sql.*;
+import java.util.Optional;
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,27 +17,64 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ServletUtente")
 public class ServletUtente extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ServletUtente() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	private static final String GET_BY_NAME = "SELECT nome_utente, password_utente FROM utenti WHERE nome_utente = ? and password_utente= ?";
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public ServletUtente() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	/**
+	 * 
+	 * @author marco query sul database
+	 * 
+	 */
+	public static boolean checkUser(String user, String password) {
+		boolean accesso = false;
+		try {
+			Connection conn = Connector.getConnection(); //
+			PreparedStatement ps = conn.prepareStatement(GET_BY_NAME);
+			ps.setString(1, user);
+			ps.setString(2, password);
+			ResultSet rs = ps.executeQuery();
+			accesso = rs.next();
+
+		} catch (SQLException se) {
+			se.printStackTrace();
+		}
+		return accesso;
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String user = request.getParameter("user");
+		String password = request.getParameter("password");
+
+		if (checkUser(user, password) == true) {
+			RequestDispatcher rs = request.getRequestDispatcher("Welcome");
+			rs.forward(request, response);
+		} else {
+			System.out.println("Username or Password incorrect");
+			RequestDispatcher rs = request.getRequestDispatcher("index.html");
+			rs.include(request, response);
+		}
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
