2d08fca02092f7e51117be5a65b7b969adf9aa49
Giordano <giordano.ditullio@gmail.com>
2020-04-21 22:01:04 +0200

    servletcarrello

---
+++ src/main/java/wa/ServletCarrello.java
@@ -0,0 +1,59 @@
+package wa;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
+
+@WebServlet("/ServletCarrello")
+public class ServletCarrello extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Resource(name = "jdbc/blue")
+	private DataSource ds;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		String vino = request.getParameter("vino");
+		if (session.getAttribute("user") == null) {
+			RequestDispatcher rd = getServletContext().getRequestDispatcher("/index.jsp");
+			PrintWriter out = response.getWriter();
+			out.println("<font color=red>Effettua il login prima di procedere all'acquisto.</font>");
+			rd.include(request, response);
+		} else {
+			try (VinoDao dao = new VinoDao(ds)) {
+
+				if (session.getAttribute("carrello") instanceof List) {
+					List<Vino> lista = new ArrayList<Vino>();
+					lista=(List<Vino>) session.getAttribute("carrello");
+					
+					session.setAttribute("carrello", dao.carrello(vino, lista) );
+				}
+			}
+		}
+
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
--- src/main/java/wa/VinoDao.java
+++ src/main/java/wa/VinoDao.java
@@ -7,45 +7,68 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import javax.sql.DataSource;
 
 public class VinoDao implements Closeable {
-	 private Connection conn;
-	 private static final String GET_TYPE = "select p.nome, t.nome, p.anno, p.prezzo from prodotti p join tipi t using(tipo_id) where p.nome=?;";
-	 
-	 public VinoDao(DataSource ds) {
+	private Connection conn;
+	private static final String GET_TYPE = "select p.nome, t.nome, p.anno, p.prezzo from prodotti p join tipi t using(tipo_id) where p.nome=?;";
 
-	        try {
-	            this.conn = ds.getConnection();
-	        } catch (SQLException se) {
-	            throw new IllegalStateException("Database issue " + se.getMessage());
-	        }
-	    }
+	public VinoDao(DataSource ds) {
+
+		try {
+			this.conn = ds.getConnection();
+		} catch (SQLException se) {
+			throw new IllegalStateException("Database issue " + se.getMessage());
+		}
+	}
 
 	@Override
 	public void close() throws IOException {
 		// TODO Auto-generated method stub
-		
-	}
-	
-	public Optional<Vino> get(String nome) {
-        try (Statement stmt = conn.createStatement(); //
-                PreparedStatement ps = conn.prepareStatement(GET_TYPE)) {
-            ps.setString(1, nome);
-            
-            try (ResultSet rs = ps.executeQuery()) {
-                if (rs.next()) {
-                    Vino my = new Vino(rs.getString(1), rs.getString(2), rs.getInt(3), rs.getDouble(4));
-                    return Optional.of(my);
-                }
-            }
-        } catch (SQLException se) {
-            se.printStackTrace();
-        }
 
-        return Optional.empty();
-    }
-	 
+	}
+
+	public Optional<Vino> get(String nome) {
+		try (Statement stmt = conn.createStatement(); //
+				PreparedStatement ps = conn.prepareStatement(GET_TYPE)) {
+			ps.setString(1, nome);
+
+			try (ResultSet rs = ps.executeQuery()) {
+				if (rs.next()) {
+					Vino my = new Vino(rs.getString(1), rs.getString(2), rs.getInt(3), rs.getDouble(4));
+					return Optional.of(my);
+				}
+			}
+		} catch (SQLException se) {
+			se.printStackTrace();
+		}
+
+		return Optional.empty();
+	}
+
+	public List<Vino> carrello(String vino, List<Vino> carrello) {
+		
+		try (Statement stmt = conn.createStatement(); //
+				PreparedStatement ps = conn.prepareStatement(GET_TYPE)) {
+			ps.setString(1, vino);
+
+			try (ResultSet rs = ps.executeQuery()) {
+				if (rs.next()) {
+					Vino my = new Vino(rs.getString(1), rs.getString(2), rs.getInt(3), rs.getDouble(4));
+					carrello.add(my);
+				}
+				return carrello;
+
+			}
+		} catch (SQLException se) {
+			se.printStackTrace();
+		}
+
+		return carrello;
+	}
+
 }
