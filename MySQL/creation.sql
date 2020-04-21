40257dfcdf555a3c8ce7a006071eb4708e030ba4
marcoingrasci95 <marcoingrasci95@gmail.com>
2020-04-21 10:25:09 +0200

    creazione tabella tipi e foreign key in prodotti

--- MySQL/creation.sql
+++ MySQL/creation.sql
@@ -2,23 +2,34 @@ use blue;
 
 drop table if exists acquisti;
 drop table if exists prodotti;
+drop table if exists tipi;
 drop table if exists utenti;
+
+
+create table tipi(
+tipo_id integer primary key auto_increment,
+nome varchar(10));
+
+insert into tipi(nome) value('rosso');
+insert into tipi(nome) value('bianco');
+insert into tipi(nome) value('spumante');
 
 
 create table prodotti(
 prodotto_id integer primary key auto_increment,
-nome_prodotto varchar(20),
-tipo_di_vino varchar(10));
+nome varchar(20),
+tipo_id int,
+foreign key (tipo_id) references tipi(tipo_id));
 
-insert into prodotti(nome_prodotto,tipo_di_vino) value('Bonarda','rosso');
-insert into prodotti(nome_prodotto,tipo_di_vino) value('Lambrusco','rosso');
-insert into prodotti(nome_prodotto,tipo_di_vino) value('Negroamaro','rosso');
-insert into prodotti(nome_prodotto,tipo_di_vino) value('Chardonnay','bianco');
-insert into prodotti(nome_prodotto,tipo_di_vino) value('Lugana','bianco');
-insert into prodotti(nome_prodotto,tipo_di_vino) value('Vermentino','bianco');
-insert into prodotti(nome_prodotto,tipo_di_vino) value('Prosecco','spumante');
-insert into prodotti(nome_prodotto,tipo_di_vino) value('Champagne','spumante');
-insert into prodotti(nome_prodotto,tipo_di_vino) value('TrentoDoc','spumante');
+insert into prodotti(nome,tipo_id) value('Bonarda',1);
+insert into prodotti(nome,tipo_id) value('Lambrusco',1);
+insert into prodotti(nome,tipo_id) value('Negroamaro',1);
+insert into prodotti(nome,tipo_id) value('Chardonnay',2);
+insert into prodotti(nome,tipo_id) value('Lugana',2);
+insert into prodotti(nome,tipo_id) value('Vermentino',2);
+insert into prodotti(nome,tipo_id) value('Prosecco',3);
+insert into prodotti(nome,tipo_id) value('Champagne',3);
+insert into prodotti(nome,tipo_id) value('TrentoDoc',3);
 
 create table utenti(
 utente_id integer primary key auto_increment,
