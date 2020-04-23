1757295a9bbe2dece6b80fd6a3d30bac26efb4d6
Alexander Del Carpio <alexander.s.delcarpio@gmail.com>
2020-04-23 17:28:51 +0200

    Insert vini

--- src/main/java/wa/VinoDao.java
+++ src/main/java/wa/VinoDao.java
@@ -18,6 +18,8 @@ public class VinoDao implements Closeable {
 	private static final String INSERT_DATA_SQL = "insert into acquisti(acquisto_id, utente_id, prodotto_id, data_e_ora)"
 			+ "values (?, ?, ?, ?)";
 	private static final String UPDATE_STOCK = "update prodotti set stock=(stock-?) where prodotto_id=?";
+	private static final String INSERT_DATA_VINI = "insert into prodotti(prodotto_id, nome, prezzo, anno, tipo_id, stock)"
+			+ "values (?, ?, ?, ?, ?)";
 
 	public VinoDao(DataSource ds) {
 
@@ -34,7 +36,6 @@ public class VinoDao implements Closeable {
 
 	}
 
-		
 	public Optional<Vino> get(String nome) {
 		try (Statement stmt = conn.createStatement(); //
 				PreparedStatement ps = conn.prepareStatement(GET_BY_NAME)) {
@@ -42,7 +43,8 @@ public class VinoDao implements Closeable {
 
 			try (ResultSet rs = ps.executeQuery()) {
 				if (rs.next()) {
-					Vino my = new Vino(rs.getString(1), rs.getString(2), rs.getInt(3), rs.getDouble(4), rs.getInt(5), rs.getInt(6), 0);
+					Vino my = new Vino(rs.getString(1), rs.getString(2), rs.getInt(3), rs.getDouble(4), rs.getInt(5),
+							rs.getInt(6), 0);
 					return Optional.of(my);
 				}
 			}
@@ -54,15 +56,13 @@ public class VinoDao implements Closeable {
 	}
 
 	public void updateStock(int ordine, int id, Vino vino) throws SQLException {
-		try(Statement stmt = conn.createStatement();
-				PreparedStatement ps = conn.prepareStatement(UPDATE_STOCK)){
+		try (Statement stmt = conn.createStatement(); PreparedStatement ps = conn.prepareStatement(UPDATE_STOCK)) {
 			ps.setInt(1, ordine);
 			ps.setInt(2, id);
 		}
-		vino.setStock(vino.getStock()-ordine);
-	
+		vino.setStock(vino.getStock() - ordine);
+
 	}
-	
 
 	public void insertData(int acquisto_id, int utente_id, int prodotto_id, double data_e_ora) {
 		try (Statement stmt = conn.createStatement(); //
@@ -71,6 +71,22 @@ public class VinoDao implements Closeable {
 			ps.setInt(2, utente_id);
 			ps.setInt(3, prodotto_id);
 			ps.setDouble(4, data_e_ora);
+
+		} catch (SQLException se) {
+			se.printStackTrace();
+		}
+
+	}
+
+	public void insertVini(int prodotto_id, String nome, double prezzo, int anno, String tipo, int stock) {
+		try (Statement stmt = conn.createStatement(); //
+				PreparedStatement ps = conn.prepareStatement(INSERT_DATA_VINI)) {
+			ps.setInt(1, prodotto_id);
+			ps.setString(2, nome);
+			ps.setDouble(3, prezzo);
+			ps.setInt(4, anno);
+			ps.setString(5, tipo);
+			ps.setInt(6, stock);
 
 		} catch (SQLException se) {
 			se.printStackTrace();
